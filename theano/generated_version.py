0e3ac88e7fb58012d160b5d8ca7f53368d9ff5c7
Sam Huh <samhuhss@gmail.com>
2017-02-03 18:30:25 -0800

    theano develop docker first commit

---
+++ theano/generated_version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM THEANO SETUP.PY
+short_version = '0.9.0beta1'
+version = '0.9.0beta1'
+git_revision = '8b9f73365e4932f1c005a0a37b907d28985fbc5f'
+full_version = '0.9.0beta1.dev-%(git_revision)s' % {
+    'git_revision': git_revision}
+release = False
+
+if not release:
+    version = full_version
